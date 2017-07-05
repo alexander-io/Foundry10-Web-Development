35d833fce755ad5dd297d51b9b2f885e532165f9
alexander-io <abharris@pugetsound.edu>
2017-07-05 14:51:48 -0700

    ++wrap-up, 0705

--- serve.js
+++ serve.js
@@ -2,29 +2,14 @@ var express = require("express")
 var app = express()
 let port = 8080
 
-
 app.use(express.static('public'))
 
-app.get('/', function(req, res){
+// listen for requests from clients for index.html
+app.get('/index.html', function(req, res){
 
 })
 
-
-app.get('/programs', function(req, res){
-
-})
-
-app.get('/programs/', function(req, res){
-
-})
-
-
-// research
-app.get('/research', function(req, res){
-
-})
-
-
+// start listening infinitely
 app.listen(port, function(){
   console.log('listening for requests on', port)
 })
