232f11e28338d921a4622af0c5c7936e1f7e1e68
alexander-io <abharris@pugetsound.edu>
2017-07-25 18:12:10 -0700

    ++queue loop animation, create queue constructor so to allow generation of multiples queues for use in multiple animation loops

--- public/js/animation_loop.js
+++ public/js/animation_loop.js
@@ -2,6 +2,19 @@
   let intro_header = $('#intro-header')
   let intr_head = document.getElementById('intro-header');
 
+  let nav_header = $('#nav-header')
+  let nav_headr = document.getElementById('nav-header');
+
+  let jqs = [], jss = []
+
+  jqs.push(intro_header)
+  jqs.push(nav_header)
+
+  jss.push(intr_head)
+  jss.push(nav_headr )
+
+
+
   let lst_of_titles = [
     , 'research'
     , 'collaboration'
@@ -9,13 +22,25 @@
     , 'foundry10'
   ]
 
+  
+  class Q {
+      constructor () { this.lst = [] }
+      enqueue (elem) { this.lst.push(elem) }
+      dequeue () { return this.lst.shift() }
+      peek () { return this.lst[0] }
+      load (lst) { lst.filter((elem) => this.enqueue(elem)) }
+  }
+
+  let header_queue = new Q()
+  header_queue.load(lst_of_titles)
+
   let q = {
     lst:[],
     enqueue : function(elem) {
       if (this.lst === 'undefined') this.lst = []
       this.lst.push(elem)},
     dequeue : function() { return this.lst.shift() },
-    peek : function() { return lst[0] },
+    peek : function() { return this.lst[0] },
     load : function(lst)  {
       if (this.lst == 'undefined') this.lst = []
       lst.filter((elem) => this.enqueue(elem))
@@ -26,39 +51,73 @@
   q.load(lst_of_titles)
 
   // pass in a queue that has a loaded q.lst array (loaded with strings)
-  let animateCss = function(q , animationName) {
+  let animateCss = function(q , animationName, jq, js) {
     var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+  //
+    // jqs.forEach(function(item, index, array){
+    //   item.addClass('animated ' + animationName).one(animationEnd, function(){
+    //     // when the animation has ended, set visibility of elem to hidden & remove animation class
+    //     item.css('visibility', 'hidden')
+    //     // -webkit-backface-visibility: hidden;
+    //     item.css('-webkit-backface-visibility', 'hidden')
+    //     item.removeClass('animated ' + animationName)
+    //
+    //     // we have a list of strings to display in our queue, dequeue one
+    //     let q_elem = q.dequeue()
+    //
+    //     // set the innerhtml text content to correspond with the text we just dequeued
+    //     jss[index].innerHTML = q_elem
+    //
+    //     // place text node to back of queue
+    //     q.enqueue(q_elem)
+    //
+    //     // want to fade element back in smoothly, use opacity. set the opacity of the header elem to 0.0, flip switch back to visible
+    //     let opacity = 0.0
+    //     item.css('opacity', opacity)
+    //     // init gas on opacity as interval to increment and assign the opacity
+    //     let gas_on_opacity = setInterval(function(){
+    //       opacity += .05
+    //       item.css('opacity', opacity)
+    //       if (opacity >= 1.0) {
+    //         clearInterval(gas_on_opacity)
+    //       }
+    //     }, 25)
+    //     item.css('visibility', 'visible')
+    //     item.css('-webkit-backface-visibility', 'visible')
+    //
+    //   })
+    // })
 
     // first add the animation class to the obj, this starts the animation on func call
-    intro_header.addClass('animated ' + animationName).one(animationEnd, function(){
+    jq.addClass('animated ' + animationName).one(animationEnd, function(){
       // when the animation has ended, set visibility of elem to hidden & remove animation class
-      intro_header.css('visibility', 'hidden')
+      jq.css('visibility', 'hidden')
       // -webkit-backface-visibility: hidden;
-      intro_header.css('-webkit-backface-visibility', 'hidden')
-      intro_header.removeClass('animated ' + animationName)
+      jq.css('-webkit-backface-visibility', 'hidden')
+      jq.removeClass('animated ' + animationName)
 
       // we have a list of strings to display in our queue, dequeue one
       let q_elem = q.dequeue()
 
       // set the innerhtml text content to correspond with the text we just dequeued
-      intr_head.innerHTML = q_elem
+      js.innerHTML = q_elem
 
       // place text node to back of queue
       q.enqueue(q_elem)
 
       // want to fade element back in smoothly, use opacity. set the opacity of the header elem to 0.0, flip switch back to visible
       let opacity = 0.0
-      intro_header.css('opacity', opacity)
+      jq.css('opacity', opacity)
       // init gas on opacity as interval to increment and assign the opacity
       let gas_on_opacity = setInterval(function(){
         opacity += .05
-        intro_header.css('opacity', opacity)
+        jq.css('opacity', opacity)
         if (opacity >= 1.0) {
           clearInterval(gas_on_opacity)
         }
       }, 25)
-      intro_header.css('visibility', 'visible')
-      intro_header.css('-webkit-backface-visibility', 'visible')
+      jq.css('visibility', 'visible')
+      jq.css('-webkit-backface-visibility', 'visible')
 
     })
   }
@@ -66,6 +125,12 @@
 
   // finally, call the animateCss func every time unit
   let loop_headers = setInterval(function(){
-    animateCss(q, 'fadeOut')
+    // animateCss(q, 'fadeOut', jqs, jss)
+    animateCss(q, 'fadeOut', intro_header, intr_head)
+    animateCss(header_queue, 'fadeOut', nav_header, nav_headr)
   }, 7000)
+
+  // let l_header = setInterval(function(){
+  // }, 7000)
+
 })()
