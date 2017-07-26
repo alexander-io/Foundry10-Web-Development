fa730210eb709f822f299c7829e871e7c14bdcfa
alexander-io <abharris@pugetsound.edu>
2017-07-26 16:23:52 -0700

    ++quote_cycle, expand queue class to enqueue to head and dequeue from tail

--- public/js/quote_cycle.js
+++ public/js/quote_cycle.js
@@ -2,16 +2,23 @@
   class Q {
       constructor () { this.lst = [] }
       enqueue (elem) { this.lst.push(elem) }
+      enqueue_head (elem) { this.lst.unshift(elem) }
       dequeue () { return this.lst.shift() }
+      dequeue_tail() {
+        this.lst.reverse()
+        let tail = this.lst.shift()
+        this.lst.reverse()
+        return tail
+      }
       peek () { return this.lst[0] }
       load (lst) { lst.filter((elem) => this.enqueue(elem)) }
   }
 
 
   let lst_of_quotes = [
-    '<color style="color:#ff6600;">"</color><quote-text id="quote-text">We believe in transforming thoughts and ideas into actionable outcomes for students and the communities that support them. </quote-text><color style="color:#ff6600">"</color>'
+    '<color style="color:#ff6600;">"</color><quote-text id="quote-text">A philanthropic research organization promoting education in non-traditional settings and subjects.</quote-text><color style="color:#ff6600">"</color>'
+    ,'<color style="color:#ff6600;">"</color><quote-text id="quote-text">We believe in transforming thoughts and ideas into actionable outcomes for students and the communities that support them. </quote-text><color style="color:#ff6600">"</color>'
     ,'<color style="color:#ff6600;">"</color><quote-text id="quote-text">We work cooperatively to empower teacher creativity and enhance both teacher and student ideas.</quote-text><color style="color:#ff6600">"</color>'
-    ,'<color style="color:#ff6600;">"</color><quote-text id="quote-text">A philanthropic research organization promoting education in non-traditional settings and subjects.</quote-text><color style="color:#ff6600">"</color>'
   ]
   /*
 The people who know kids best are the ones who work closely with them. With this in mind, we work cooperatively to empower teacher creativity and enhance both teacher and student ideas.
@@ -30,6 +37,8 @@ The people who know kids best are the ones who work closely with them. With this
   , left_arrow = document.getElementById('left-arrow')
   , right_arrow = document.getElementById('right-arrow')
 
+
+
   let animateCss = function (animationName) {
 
     var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
@@ -44,7 +53,9 @@ The people who know kids best are the ones who work closely with them. With this
   let quote_queue = new Q()
   quote_queue.load(lst_of_quotes)
 
-
+  let upd = quote_queue.dequeue()
+  quote_text_header.innerHTML = upd
+  quote_queue.enqueue(upd)
 
   left_panel.style.cursor = "pointer"
   right_panel.style.cursor = "pointer"
@@ -89,9 +100,13 @@ The people who know kids best are the ones who work closely with them. With this
     setTimeout(function(){
       // TODO : modify queue and update innerhtml content
 
-      let update = quote_queue.dequeue()
+      let update = quote_queue.dequeue_tail()
+      quote_queue.enqueue_head(update)
+      update = quote_queue.dequeue_tail()
       quote_text_header.innerHTML = update
       quote_queue.enqueue(update)
+
+
       animateCss('slideInLeft')
       quote_text_header_jq.fadeTo(750, 1)
     }, 1050)
