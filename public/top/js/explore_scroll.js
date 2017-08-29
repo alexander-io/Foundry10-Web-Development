f5dced37611b9f0ea63ecb077b1ccc3d618e5cef
alexander-io <abharris@pugetsound.edu>
2017-08-28 18:50:48 -0700

    ++add top section

---
+++ public/top/js/explore_scroll.js
@@ -0,0 +1,55 @@
+(function(){
+  let explore_research_large = document.getElementById('explore-research-large'),
+    explore_programs_large = document.getElementById('explore-programs-large'),
+    explore_outreach_large = document.getElementById('explore-outreach-large'),
+    explore_research_small = document.getElementById('explore-research-small'),
+    explore_programs_small = document.getElementById('explore-programs-small'),
+    explore_outreach_small = document.getElementById('explore-outreach-small')
+
+  let research_target = document.getElementById('s1'),
+    programs_target = document.getElementById('s2'),
+    outreach_target = document.getElementById('s3')
+
+  let research_target_rect = research_target.getBoundingClientRect(),
+    programs_target_rect = programs_target.getBoundingClientRect(),
+    outreach_target_rect = outreach_target.getBoundingClientRect()
+
+  let research_target_offset = research_target_rect.top,
+    programs_target_offset = programs_target_rect.top,
+    outreach_target_offset = outreach_target_rect.top
+
+  let lst_of_exploration = []
+  lst_of_exploration.push(
+    explore_research_large,
+    explore_programs_large,
+    explore_outreach_large,
+    explore_research_small,
+    explore_programs_small,
+    explore_outreach_small
+  )
+
+  explore_research_large.addEventListener('click', function(e) {
+    $('html,body').animate({scrollTop:research_target_offset}, 900)
+  });
+
+  explore_research_small.addEventListener('click', function(){
+    $('html,body').animate({scrollTop:research_target_offset}, 900)
+  })
+
+  explore_programs_large.addEventListener('click', function(){
+    $('html,body').animate({scrollTop:programs_target_offset}, 900)
+  })
+
+  explore_programs_small.addEventListener('click', function(){
+    $('html,body').animate({scrollTop:programs_target_offset}, 900)
+  })
+
+  explore_outreach_large.addEventListener('click', function(){
+    $('html,body').animate({scrollTop:outreach_target_offset}, 900)
+  })
+
+  explore_outreach_small.addEventListener('click', function(){
+    $('html,body').animate({scrollTop:outreach_target_offset}, 900)
+  })
+
+})()
--- serve.js
+++ serve.js
@@ -4,10 +4,6 @@ let port = 8080
 
 app.use(express.static('public'))
 
-// listen for requests from clients for index.html
-// app.get('/index.html', function(req, res){
-// })
-
 // start listening infinitely
 app.listen(port, function(){
   console.log('listening for requests on', port)
